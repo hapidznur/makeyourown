adb69875368098517f0a4a9ab1db7fe33e6c8be3
hafid hidayatullah <hapidznur@gmail.com>
2023-11-04 23:24:40 +0700

    add negative id and cek length column

--- db.c
+++ db.c
@@ -11,8 +11,8 @@
 
 typedef struct{
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE+1];
+    char email[COLUMN_EMAIL_SIZE+1];
 } Row;
 
 
@@ -40,7 +40,7 @@ typedef enum {
 } MetaCommonResult;
 
 typedef enum {
-PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT, PREPARE_SYNTAX_ERROR
+PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT, PREPARE_SYNTAX_ERROR, PREPARE_STRING_TO_LONG,PREPARE_NEGATIVE_ID,
 } PrepareResult;
 
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
@@ -143,15 +143,40 @@ MetaCommonResult do_meta_command(InputBuffer* input_buffer, Table *table){
     }
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+  statement->type = STATEMENT_INSERT;
+  
+  char* keyword = strtok(input_buffer->buffer, " ");
+  char* id_string = strtok(NULL, " ");
+  char* username = strtok(NULL, " ");
+  char* email = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL){
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if (id < 0) {
+    return PREPARE_NEGATIVE_ID;
+  }
+  if (strlen(username) > COLUMN_USERNAME_SIZE){
+    return PREPARE_STRING_TO_LONG;
+  }
+
+  if (strlen(email) > COLUMN_EMAIL_SIZE){
+    return PREPARE_STRING_TO_LONG;
+  }
+  
+  statement->row_to_insert.id = id; 
+  strcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if(strncmp(input_buffer->buffer, "insert", 6) == 0){
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        } 
-        return PREPARE_SUCCESS;
+      return prepare_insert(input_buffer, statement);
     }
     // printf("Syntex errro %d, %s", strncmp(input_buffer, "insert", 6), input_buffer->buffer);
     if(strcmp(input_buffer->buffer, "select") == 0){
@@ -211,6 +236,12 @@ int main(int argc, char* argv[]){
         {
         case (PREPARE_SUCCESS):
             break;
+        case (PREPARE_STRING_TO_LONG):
+            printf("String is to long.\n");
+            continue;
+        case (PREPARE_NEGATIVE_ID):
+            printf("ID must be positive.\n");
+            continue;
         case (PREPARE_SYNTAX_ERROR):
             printf("Syntax error");
             continue;
@@ -225,7 +256,7 @@ int main(int argc, char* argv[]){
             printf("Executed.\n");
             break;
         case (EXECUTE_TABLE_FULL):
-            printf("ERROR: table full");
+            printf("Error: Table full.\n");
             break;
         }
     }
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -26,4 +26,60 @@ describe 'database' do
       "db > .exit",
     ])
   end
+
+  it 'prints error message when table is full' do
+    script = (1..1401).map do|i|
+      "insert #{i} user#{i} person#{i}@example.com"
+    end
+    script << ".exit"
+    result = run_script(script)
+    expect(result[-2]).to eq('db > Error: Table full.')
+  end
+
+  it 'allows inserting strings that are the maximum length' do
+    long_username = "a"*32
+    long_email = "a"*255
+    script = [
+      "insert 1 #{long_username} #{long_email}",
+      "select", 
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to match_array([
+      "db > Executed.",
+      "db > (1, #{long_username},#{long_email})",
+      "Executed.",
+      "db > .exit",
+    ])
+  end
+
+  it 'print error message if string are to long' do
+    long_username = "a"*33
+    long_email = "a"*256
+    script = [
+      "insert 1 #{long_username} #{long_email}",
+      "select", 
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to match_array([
+      "db > String is to long.",
+      "db > Executed.",
+      "db > .exit",
+    ])
+  end
+
+  it 'prints an error message if id is negative' do
+    script = [
+      "insert -1 cstack fo@bar.com",
+      "select", 
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to match_array([
+      "db > ID must be positive.",
+      "db > Executed.",
+      "db > .exit",
+    ])
+  end
 end
