454247efbe3c2cfdab34bf3e81786837a2a24206
hafid hidayatullah <hapidznur@gmail.com>
2023-11-04 22:24:38 +0700

    add test

--- db.c
+++ db.c
@@ -6,8 +6,8 @@
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
-
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute);
+#define TABLE_MAX_PAGES 100
 
 typedef struct{
     uint32_t id;
@@ -25,7 +25,6 @@ const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 const uint32_t PAGE_SIZE = 4096;
-#define TABLE_MAX_PAGES 100
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
@@ -213,7 +212,7 @@ int main(int argc, char* argv[]){
         case (PREPARE_SUCCESS):
             break;
         case (PREPARE_SYNTAX_ERROR):
-            printf("Syntex errro");
+            printf("Syntax error");
             continue;
         case (PREPARE_UNRECOGNIZED_STATEMENT):
             printf("Unrecognized keyword '%s' .\n", input_buffer->buffer);
