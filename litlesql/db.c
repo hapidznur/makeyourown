36089f56fa5b94070ac6f2c1f947b06583a41b04
hafid hidayatullah <hapidznur@gmail.com>
2023-11-28 15:12:09 +0700

    change finding insert key

--- litlesql/db.c
+++ litlesql/db.c
@@ -7,7 +7,6 @@
 #include <stdint.h>
 #include <unistd.h>
 
-
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute);
@@ -74,6 +73,7 @@ typedef enum { STATEMENT_INSERT, STATEMENT_SELECT} StatementType;
 
 typedef enum {
     EXECUTE_SUCCESS,
+    EXECUTE_DUPLICATE_KEY,
     EXECUTE_TABLE_FULL
 } ExecuteResult;
 
@@ -127,6 +127,7 @@ void deserialize_row(void* source, Row* destination){
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+// NoodeType
 uint32_t* leaf_node_num_cells(void* node){
   return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
@@ -142,8 +143,20 @@ uint32_t* leaf_node_key(void* node, uint32_t cell_num){
 uint32_t* leaf_node_value(void* node, uint32_t cell_num){
   return leaf_node_cell(node,cell_num) + LEAF_NODE_KEY_SIZE;
 }
+NodeType get_node_type(void* node){
+  uint8_t value = *((uint8_t*)(node+NODE_TYPE_OFFSET));
+  return (NodeType)value;
+}
 
-void initialize_leaf_node(void* node) {*leaf_node_num_cells(node) =0;}
+void set_node_type(void* node, NodeType type){
+  uint8_t value = type;
+  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+// end
+
+void initialize_leaf_node(void* node) {
+  set_node_type(node, NODE_LEAF);
+  *leaf_node_num_cells(node) =0;}
 
 void pager_flush(Pager* pager, uint32_t page_num) {
   if (pager->pages[page_num] == NULL) {
@@ -196,8 +209,6 @@ void* get_page(Pager* pager, uint32_t page_num){
 
   return pager->pages[page_num];
 }
-
-
 // Cursor
 Cursor* table_start(Table* table){
   Cursor* cursor = malloc(sizeof(Cursor));
@@ -211,16 +222,50 @@ Cursor* table_start(Table* table){
   return cursor;
 }
 
-Cursor* table_end(Table* table){
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_cells = *leaf_node_num_cells(node);
+
   Cursor* cursor = malloc(sizeof(Cursor));
   cursor->table = table;
-  cursor->page_num = table->root_page_num;
- 
-  void* root_node = get_page(table->pager, table->root_page_num);
-  uint32_t num_cells = *leaf_node_num_cells(root_node);
-  cursor->cell_num = num_cells;
-  cursor->end_of_table = true;
+  cursor->page_num = page_num;
+
+  // Binary search
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
+  while(one_past_max_index != min_index){
+    uint32_t index = (min_index + one_past_max_index)/2;
+    uint32_t key_at_index = *leaf_node_key(node,index);
+    if (key == key_at_index){
+      cursor->cell_num = index;
+      return cursor;
+    }
+    if (key < key_at_index){
+      one_past_max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+  cursor->cell_num = min_index;
   return cursor;
+}
+/* Return the position of the giuve key
+* it the key is not present, return the position
+* where it should be inserted
+*/
+
+Cursor* table_find(Table* table, uint32_t key){
+  uint32_t root_page_num = table->root_page_num;
+  void* root_node = get_page(table->pager, root_page_num);
+      
+  if (get_node_type(root_node) == NODE_LEAF){
+ //   printf(leaf_node_find(table, root_page_num, key));
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
+
 }
 
 void* cursor_value(Cursor* cursor){
@@ -237,8 +282,8 @@ void cursor_advance(Cursor* cursor){
     cursor->end_of_table = true;
   }
 }
-// End Cursor
 
+// End Cursor
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value){
   void* node = get_page(cursor->table->pager,cursor->page_num);
   uint32_t num_cells = *leaf_node_num_cells(node);
@@ -257,8 +302,10 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value){
   }
   *(leaf_node_num_cells(node)) +=1;
   *(leaf_node_key(node, cursor->cell_num)) = key;
+  printf("%d", cursor->cell_num);
   serialize_row(value, leaf_node_value(node, cursor->cell_num));
 }
+
 Pager* pager_open(const char* filename){
 
   int fd = open(filename, 
@@ -357,7 +404,6 @@ void print_constants(){
 
 void print_leaf_node(void* node){
   uint32_t num_cells = *leaf_node_num_cells(node);
-  printf("leaf (size %d)\n", num_cells);
   for (uint32_t i=0; i < num_cells; i++){
     uint32_t key = *leaf_node_key(node,i);
     printf(" - %d : %d\n", i, key);
@@ -374,7 +420,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table){
       print_leaf_node(get_page(table->pager, 0));
       return META_COMMAND_SUCCESS;
     } else if (strcmp(input_buffer->buffer, ".constants") == 0){
-      printf("Constants: \n");
+      printf("Constants:\n");
       print_constants();
       return META_COMMAND_SUCCESS;
     } else{
@@ -427,12 +473,20 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 
 ExecuteResult execute_insert(Statement* statement, Table* table){
   void *node = get_page(table->pager, table->root_page_num);
-  if ((*leaf_node_num_cells(node)>=LEAF_NODE_MAX_CELLS)){
+  uint32_t num_cells = (*leaf_node_num_cells(node));
+  if (num_cells >= LEAF_NODE_MAX_CELLS){
       return EXECUTE_TABLE_FULL;
   }
 
   Row* row_to_insert = &(statement->row_to_insert);
-  Cursor* cursor = table_end(table);
+  uint32_t key_to_insert = row_to_insert->id;
+  Cursor* cursor = table_find(table, key_to_insert);
+  if (cursor->cell_num < num_cells) {
+    uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+    if (key_at_index == key_to_insert){
+      return EXECUTE_DUPLICATE_KEY;
+    }
+  }
   leaf_node_insert(cursor,row_to_insert->id, row_to_insert);
   return EXECUTE_SUCCESS;
 }
@@ -512,9 +566,12 @@ int main(int argc, char* argv[]){
       case (EXECUTE_SUCCESS):
           printf("Executed.\n");
           break;
+      case (EXECUTE_DUPLICATE_KEY):
+          printf("Error: Duplicate key.\n");
+          break;
       case (EXECUTE_TABLE_FULL):
           printf("Error: Table full.\n");
           break;
       }
     }
-}
+};
