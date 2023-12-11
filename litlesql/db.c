659d86de15de754061694b84daf18fc7508351d3
hafid hidayatullah <hapidznur@gmail.com>
2023-12-11 14:42:14 +0700

    search binary tree

--- litlesql/db.c
+++ litlesql/db.c
@@ -262,82 +262,6 @@ void* get_page(Pager* pager, uint32_t page_num){
  * go onot the end of the database file
  */
 uint32_t get_unused_page_num(Pager* pager){return pager->num_pages;}
-
-// Cursor
-Cursor* table_start(Table* table){
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->page_num = table->root_page_num;
-  cursor->cell_num = 0;
-
-  void* root_node = get_page(table->pager, table->root_page_num);
-  uint32_t num_cells = *leaf_node_num_cells(root_node);
-  cursor->end_of_table = (num_cells == 0);
-  return cursor;
-}
-
-Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
-  void* node = get_page(table->pager, page_num);
-  uint32_t num_cells = *leaf_node_num_cells(node);
-
-  Cursor* cursor = malloc(sizeof(Cursor));
-  cursor->table = table;
-  cursor->page_num = page_num;
-
-  // Binary search
-  uint32_t min_index = 0;
-  uint32_t one_past_max_index = num_cells;
-  while(one_past_max_index != min_index){
-    uint32_t index = (min_index + one_past_max_index)/2;
-    uint32_t key_at_index = *leaf_node_key(node,index);
-    if (key == key_at_index){
-      cursor->cell_num = index;
-      return cursor;
-    }
-    if (key < key_at_index){
-      one_past_max_index = index;
-    } else {
-      min_index = index + 1;
-    }
-  }
-  cursor->cell_num = min_index;
-  return cursor;
-}
-/* Return the position of the giuve key
-* it the key is not present, return the position
-* where it should be inserted
-*/
-
-Cursor* table_find(Table* table, uint32_t key){
-  uint32_t root_page_num = table->root_page_num;
-  void* root_node = get_page(table->pager, root_page_num);
-      
-  if (get_node_type(root_node) == NODE_LEAF){
- //   printf(leaf_node_find(table, root_page_num, key));
-    return leaf_node_find(table, root_page_num, key);
-  } else {
-    printf("Need to implement searching an internal node\n");
-    exit(EXIT_FAILURE);
-  }
-
-}
-
-void* cursor_value(Cursor* cursor){
-  uint32_t page_num = cursor->page_num;
-  void* page = get_page(cursor->table->pager, page_num);
-  return leaf_node_value(page,cursor->cell_num);
-}
-
-void cursor_advance(Cursor* cursor){
-  uint32_t page_num = cursor->page_num;
-  void* node = get_page(cursor->table->pager, page_num);
-  cursor->cell_num +=1;
-  if (cursor->cell_num >= (*leaf_node_num_cells(node))){
-    cursor->end_of_table = true;
-  }
-}
-
-// End Cursor
 // Internal Node
 
 uint32_t* internal_node_num_keys(void* node) {
@@ -377,7 +301,110 @@ uint32_t get_node_max_key(void* node) {
       return *leaf_node_key(node, *leaf_node_num_cells(node)-1);
   }
 }
-//
+
+// Cursor
+Cursor* table_start(Table* table){
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = table->root_page_num;
+  cursor->cell_num = 0;
+
+  void* root_node = get_page(table->pager, table->root_page_num);
+  uint32_t num_cells = *leaf_node_num_cells(root_node);
+  cursor->end_of_table = (num_cells == 0);
+  return cursor;
+}
+
+
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_cells = *leaf_node_num_cells(node);
+
+  Cursor* cursor = malloc(sizeof(Cursor));
+  cursor->table = table;
+  cursor->page_num = page_num;
+
+  // Binary search
+  uint32_t min_index = 0;
+  uint32_t one_past_max_index = num_cells;
+  while(one_past_max_index != min_index){
+    uint32_t index = (min_index + one_past_max_index)/2;
+    uint32_t key_at_index = *leaf_node_key(node,index);
+    if (key == key_at_index){
+      cursor->cell_num = index;
+      return cursor;
+    }
+    if (key < key_at_index){
+      one_past_max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+  cursor->cell_num = min_index;
+  return cursor;
+}
+/* Return the position of the giuve key
+* it the key is not present, return the position
+* where it should be inserted
+*/
+
+Cursor* internal_node_find(Table* table, uint32_t page_num, uint32_t key){
+  void* node = get_page(table->pager, page_num);
+  uint32_t num_keys = *internal_node_num_keys(node);
+
+  /* Binary search to find index of child to search*/
+  uint32_t min_index = 0;
+  uint32_t max_index = num_keys; /*one more child than key*/
+
+  while(min_index != max_index){
+    uint32_t index = (min_index+max_index)/2;
+    uint32_t key_to_right = *internal_node_key(node, index);
+    if (key_to_right >= key){
+      max_index = index;
+    } else {
+      min_index = index + 1;
+    }
+  }
+
+  uint32_t child_num = *internal_node_child(node, min_index);
+  void* child = get_page(table->pager, child_num);
+  switch (get_node_type(child)) {
+  case NODE_LEAF:
+    return leaf_node_find(table, child_num, key);
+  case NODE_INTERNAL:
+      return internal_node_find(table, child_num, key);
+  }
+}
+Cursor* table_find(Table* table, uint32_t key){
+  uint32_t root_page_num = table->root_page_num;
+  void* root_node = get_page(table->pager, root_page_num);
+      
+  if (get_node_type(root_node) == NODE_LEAF){
+ //   printf(leaf_node_find(table, root_page_num, key));
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    return internal_node_find(table, root_page_num, key);
+  }
+
+}
+
+void* cursor_value(Cursor* cursor){
+  uint32_t page_num = cursor->page_num;
+  void* page = get_page(cursor->table->pager, page_num);
+  return leaf_node_value(page,cursor->cell_num);
+}
+
+void cursor_advance(Cursor* cursor){
+  uint32_t page_num = cursor->page_num;
+  void* node = get_page(cursor->table->pager, page_num);
+  cursor->cell_num +=1;
+  if (cursor->cell_num >= (*leaf_node_num_cells(node))){
+    cursor->end_of_table = true;
+  }
+}
+
+// End Cursor
+
 void create_new_root(Table* table, uint32_t right_child_page_num){
   /*
    * Handle splitting the root.
--- litlesql/spec/main_spec.rb
+++ litlesql/spec/main_spec.rb
@@ -7,7 +7,11 @@ describe 'database' do
     raw_output = nil
     IO.popen('./db test.db', 'r+') do |pipe|
       commands.each do |command|
-        pipe.puts command
+        begin
+          pipe.puts command
+        rescue Errno:EPIPE
+           break
+        end
       end
       pipe.close_write
 
@@ -32,12 +36,15 @@ describe 'database' do
   end
 
   it 'prints error message when table is full' do
-    script = (1..1401).map do|i|
+    script = (1..1401).map do |i|
       "insert #{i} user#{i} person#{i}@example.com"
     end
     script << ".exit"
     result = run_script(script)
-    expect(result[-2]).to eq('db > Error: Table full.')
+    expect(result.last(2)).to match([
+        "db > Executed.",
+        "db > Need to implement updating parent after split"
+    ])
   end
 
   it 'allows inserting strings that are the maximum length' do
@@ -191,6 +198,8 @@ describe 'database' do
       "    - 12",
       "    - 13",
       "    - 14",
+      "db > Executed.",
+      "db > ",
     ])
   end
 end
